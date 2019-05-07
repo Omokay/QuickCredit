d4b7551884994d5404f72e4106cfc864916767b6
Omokay <chuku.omoke@gmail.com>
2019-05-07 23:18:22 +0100

    feature(authentication): authenticate signin password [Delivers #165735871]

---
+++ server/authentication/auth.js
@@ -0,0 +1,20 @@
+import bcryptjs from 'bcryptjs';
+import jsonwebtoken from 'jsonwebtoken';
+import config from 'config';
+
+class Authenticate {
+  static checkPassword(inputPassword, savedPassword) {
+    return bcryptjs.compareSync(inputPassword, savedPassword);
+  }
+
+  /** static generateToken(payload) {
+    return jsonwebtoken.sign({ payload }, config.get('jwtPrivateKey'), { expires: '10d' });
+  } */
+
+  /** static verifyToken(token) {
+    return jsonwebtoken.verify(token, process.env.SECRETKEY);
+  } */
+}
+
+
+module.export = Authenticate;
