5f432aa137ccf43be7d360bc3745be0f5b02faf8
Omokay <chuku.omoke@gmail.com>
2019-05-07 23:42:41 +0100

    feature(signin):use signin api routes [Delivers #165735871]

--- server/app.js
+++ server/app.js
@@ -1,9 +1,23 @@
+import config from 'config';
 import express from 'express';
+import parser from 'body-parser';
+import routes from './api-routes/user-routes';
 
 const app = express();
 
-const server = app.listen(process.env.port || 4000, () => {
-  console.log('Now listening on port 4000');
-});
+if (!config.get('jwtPrivateKey')) {
+  console.error('FATAL ERROR: jwtPrivateKey is not defined');
+  process.exit(1);
+}
+// allows access to body of the request
+app.use(routes);
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
 
-module.exports = server;
+// use User routes
+app.use('/api/v1', routes);
+
+
+app.listen(process.env.port || 4000, () => {
+  console.log('now listening on port 4000');
+});
