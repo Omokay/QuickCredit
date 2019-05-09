da0e7045b582b38dd884fef2f5b83f33e24b4b01
Omokay <chuku.omoke@gmail.com>
2019-05-09 21:29:09 +0100

    feature(validation):update validation for signin endpoints [Fixes #165735871]

--- server/middlewares/validation.js
+++ server/middlewares/validation.js
@@ -10,7 +10,7 @@ class Validation {
         .trim()
         .lowercase()
         .required(),
-      password: joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
+      password: joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required(),
     };
     return joi.validate(info, schema);
   }
