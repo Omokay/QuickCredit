1ef79b272b12fe3c64f81620d837731ab29345c9
Omokay <chuku.omoke@gmail.com>
2019-05-11 12:58:56 +0100

    feature(middleware):validate user inputs for signup [Delivers #165735879]

--- server/middlewares/validation.js
+++ server/middlewares/validation.js
@@ -2,7 +2,31 @@ import joi from '@hapi/joi';
 
 
 class Validation {
-// validate for user login
+  // validate for user signup
+  static signupValidation(info) {
+    const schema = {
+      email: joi.string()
+        .email()
+        .trim()
+        .lowercase()
+        .required(),
+      firstName: joi.string()
+        .min(2)
+        .required(),
+      lastName: joi.string()
+        .min(2)
+        .required(),
+      password: joi.string()
+        .regex(/^[a-zA-Z0-9]{6,30}$/)
+        .required(),
+      address: joi.string()
+        .min(10)
+        .required(),
+    };
+    return joi.validate(info, schema);
+  }
+
+  // validate for user login
   static signinValidation(info) {
     const schema = {
       email: joi.string()
