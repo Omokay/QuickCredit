1c9bd90ec4036f6b8ca7ed74a0d410ba8400e5be
Omokay <chuku.omoke@gmail.com>
2019-05-12 11:24:28 +0100

    feature(routes):add api endpoint to mark user as verified [Starts #165736023]

--- server/api-routes/user-routes.js
+++ server/api-routes/user-routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import userHandler from '../controllers/userHandler';
+import Auth from '../authentication/auth';
 
 const router = express.Router();
 
@@ -8,6 +9,7 @@ router.post('/auth/signup', userHandler.signupHandler);
 // Make post requests to signin API Endpoints
 router.post('/auth/signin', userHandler.signinHandler);
 // Make post request to verify user
-router.patch('/users/:userEmail/verify', userHandler.verifyUserHandler);
+router.patch('/users/:user-email/verify', Auth.userAuthorize, userHandler.getVerified);
+
 
 module.exports = router;
