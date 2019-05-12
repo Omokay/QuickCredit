c5a6a12c8e3d0a345f604667176d4ea4fb8e0c41
Omokay <chuku.omoke@gmail.com>
2019-05-12 11:29:19 +0100

    feature(authentication):add authorization to mark user as verified [Delivers #165736023]

--- server/authentication/auth.js
+++ server/authentication/auth.js
@@ -18,6 +18,27 @@ class Authenticate {
   static hashPassword(password) {
     return bcryptjs.hashSync(password, 10);
   }
+
+  static userAuthorize(req, res, next) {
+    try {
+      const payload = req.headers.authorization.split(' ')[1];
+      req.user = Authenticate.verifyToken(payload, config.get('jwtPrivateKey'));
+      if (req.user.email !== 'admin@quickcredit.com') {
+        return res.status(401)
+          .json({
+            status: 403,
+            error: 'Unauthorized access',
+          });
+      }
+      return next();
+    } catch {
+      return res.status(401)
+        .json({
+          status: 401,
+          error: 'Invalid token!',
+        });
+    }
+  }
 }
 
 
