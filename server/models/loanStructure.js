f186e8a06e1794dd42c6a43be4c8ae0da009d86b
Omokay <chuku.omoke@gmail.com>
2019-05-12 12:58:01 +0100

    feature(models):create model for loan structure [Delivers #165736048]

---
+++ server/models/loanStructure.js
@@ -0,0 +1,41 @@
+const loans = [
+  {
+    id: 1,
+    user: 'chuk.omoke@gmail.com',
+    createdOn: '17/03/2019 2:30am',
+    status: 'pending',
+    repaid: false,
+    tenor: 12,
+    amount: 500000,
+    paymentInstallment: 40000,
+    balance: 500000.00,
+    interest: 10000.00,
+  },
+  {
+    id: 2,
+    user: 'chuk2.omoke@gmail.com',
+    createdOn: '17/03/2019 2:30am',
+    status: 'pending',
+    repaid: false,
+    tenor: 12,
+    amount: 500000,
+    paymentInstallment: 40000,
+    balance: 500000.00,
+    interest: 10000.00,
+  },
+  {
+    id: 3,
+    user: 'chuk3.omoke@gmail.com',
+    createdOn: '17/03/2019 2:30am',
+    status: 'pending',
+    repaid: false,
+    tenor: 12,
+    amount: 500000,
+    paymentInstallment: 40000,
+    balance: 500000.00,
+    interest: 10000.00,
+  },
+];
+
+export default loans;
+
