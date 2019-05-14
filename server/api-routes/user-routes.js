63a993949b14fdb9a96fe1d663ed5865cc457e96
Omokay <chuku.omoke@gmail.com>
2019-05-15 00:50:56 +0100

    feature(routes):add routes for getting user loan repayments [Starts #165735960]

--- server/api-routes/user-routes.js
+++ server/api-routes/user-routes.js
@@ -10,16 +10,19 @@ router.post('/auth/signup', userHandler.signupHandler);
 // Make post requests to signin API Endpoints
 router.post('/auth/signin', userHandler.signinHandler);
 // Make post request to verify user
-router.patch('/users/:email/verify', Auth.userAuthorize, userHandler.getVerified);
+router.patch('/users/:email/verify', Auth.adminAuthorize, userHandler.getVerified);
 // Admin Get specific loan
 router.get('/loans/:id', Auth.userAuthorize, adminHandler.getSpecificLoan);
 
 /** This route will handle admin getting all loan and
- *  Admin getting current loans in the case where query params {status:approved & repaid:false} 
+ *  Admin getting current loans in the case where query params {status:approved & repaid:false}
  * (GET /loans?status=approved&repaid=false)
  *  Also repaid loans in the case where {status:approved & repaid:true} is passed as query params
  * (GET /loans?status=approved&repaid=true)
  */
-router.get('/loans', Auth.userAuthorize, adminHandler.getCurrentLoans);
+router.get('/loans', Auth.adminAuthorize, adminHandler.getCurrentLoans);
+
+// Get request to user repayment loan history
+router.get('/loans/:id/repayments', Auth.userAuthorize, userHandler.getRepaymentLoans);
 
 module.exports = router;
